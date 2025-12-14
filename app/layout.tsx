8b1da00aae4c78b37fef3a7da00db3d52be1a956
Curiouspods <Curiouspods@gmail.com>
2025-12-14 17:55:51 +0530

    layout changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,10 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
+        url: "/vercel.svg",
+        type: "image/svg+xml",
+      },
+      {
         url: "/icon-light-32x32.png",
         media: "(prefers-color-scheme: light)",
       },
